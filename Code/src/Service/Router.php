8eb50c093aaf496c43dbb7d81230a8b091173a1d
Marc-Alban <millet.marcalban@gmail.com>
2020-09-24 08:55:09 +0200

    Amelioration du routeur

--- Code/src/Service/Router.php
+++ Code/src/Service/Router.php
@@ -50,33 +50,40 @@ final class Router
         $pathControllerFront = 'App\Controller\Frontoffice\\'.$this->pageMaj.'Controller';
         $pathControllerBack = 'App\Controller\Backoffice\\'.$this->pageMaj.'Controller';
         $pathManager = 'App\Model\Manager\\'.$this->pageMaj.'Manager';
-        
-        $path = 'App\Model\Repository\\'.$this->pageMaj.'Repository.php';
+        $path = ROOT.'src\Model\Repository\\'.$this->pageMaj.'Repository.php';
+
+        $namePageRepo = [
+            'Inscription' => 'User',
+            'Connexion' => 'User',
+        ];
+
         if(file_exists($path)){
-            $pathRepository = 'App\Model\Repository\\'.$this->pageMaj.'Repository';     
+            $pathRepository = $path;
         }else if(!file_exists($path)){
-            $pathRepository = 'App\Model\Repository\\'.$this->pageMaj.'Repository';
+            if(array_key_exists($this->pageMaj, $namePageRepo)){
+                $pathRepository = ROOT.'src\Model\Repository\\'.$namePageRepo[$this->pageMaj].'Repository.php';
+            }else if(array_key_exists($this->pageMaj, $namePageRepo) === false ){
+                $pathRepository = $path;
+            }
         }
 
         if(in_array($this->pageMaj, $this->pageFront) || empty($this->pageMaj) || !in_array($this->pageMaj, $this->pageBack))
         {
             if($arg === 'controller'){
                 return $pathControllerFront;
-            }else if($arg === 'manager'){
-                return $pathManager;
-            }else if($arg === 'repository'){
-                return $pathRepository;
             }
         }
         else if(in_array($this->pageMaj, $this->pageBack))
         {
             if($arg === 'controller'){
                 return $pathControllerBack;
-            }else if($arg === 'manager'){
-                return $pathManager;
-            }else if($arg === 'repository'){
-                return $pathRepository;
             }
+        }
+
+        if($arg === 'manager'){
+            return $pathManager;
+        }else if($arg === 'repository'){
+            return $pathRepository;
         }
     }
 /************************************End Controller************************************************* */
@@ -87,8 +94,16 @@ final class Router
         $controllerClass = $this->page('controller');
         $managerClass = $this->page('manager');
         $repoClass = $this->page('repository');
-        $repo = new $repoClass($this->database);
-        $manager = new $managerClass($repo,['token' => $this->token, 'session' => $this->session]);
+        
+        if(file_exists($repoClass)){
+            $pathApp = str_replace('src', 'App', $repoClass);
+            $subPath = substr_replace($pathApp, '', 0, -39);
+            $pathRepo = str_replace('.php', '', $subPath);
+            $repo = new $pathRepo($this->database);
+            $manager = new $managerClass($repo,['token' => $this->token, 'session' => $this->session]);
+        }elseif(!file_exists($repoClass)){
+            $manager = new $managerClass(['token' => $this->token, 'session' => $this->session]);
+        }
         $controllerObject = new $controllerClass($manager, ['view' => $this->view, 'error' => $this->error, 'token' => $this->token, 'session' => $this->session]);
         $methode = $this->pageMaj.'Action';
         return $controllerObject->$methode($datas);
